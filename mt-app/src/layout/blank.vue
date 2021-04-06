832c06e922c9cf77b58ba7c329bebf398094bb0c
linpeng <2603230766@qq.com>
2021-04-06 21:05:12 +0800

    修改了div为info

--- mt-app/src/layout/blank.vue
+++ mt-app/src/layout/blank.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="page-blank">
         <div>我修改了东西</div>
+        <!-- info -->
+        <div>info</div>
         <!-- 注册页面 -->
         <div>我是新增的东西</div>
         <router-view></router-view>
