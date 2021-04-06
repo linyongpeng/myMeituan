e78fd688c2521ec359178cbd2d89d321140a82d9
linpeng <2603230766@qq.com>
2021-04-06 21:08:26 +0800

    233

--- mt-app/src/layout/blank.vue
+++ mt-app/src/layout/blank.vue
@@ -2,7 +2,7 @@
     <div class="page-blank">
         <div>我修改了东西</div>
         <!-- info 123123-->
-        <div>info 1232323</div>
+        <div>info 12333323</div>
         <!-- 注册页面 -->
         <div>我是新增的东西</div>
         <router-view></router-view>
